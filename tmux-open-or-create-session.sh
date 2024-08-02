a7033b5b20343c6f247d3731628ee5fe58da2f0c
Pavlo Skobnikov <pavlo.skobnikov@gmail.com>
2024-08-02 17:17:07 +0300

    Add tmux scripts for managing sessions

---
+++ tmux-open-or-create-session.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+## A script to either open existing or create new tmux session for the given path.
+
+session_path=$(realpath "$1")
+session_name=$(basename "$1" | tr . _)
+
+tmux_running=$(pgrep tmux)
+
+if [[ -z "$TMUX" ]] && [[ -z "$tmux_running" ]]; then
+    tmux new-session -s "$session_name" -c "$session_path"
+    exit 0
+fi
+
+if ! tmux has-session -t="$session_name" 2>/dev/null; then
+    tmux new-session -ds "$session_name" -c "$session_path"
+fi
+
+tmux switch-client -t "$session_name"
